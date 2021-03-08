892bbd8e9168b0fd8fa02e4977894161dd25e6d7
darashi <darashi@users.noreply.github.com>
2021-03-08 07:32:11 +0000

    deploy: 6c71251454a2dd537e2b1e8b544d9ed5b428c43a

---
+++ qux.js
@@ -0,0 +1,92 @@
+import { d as defineStanzaElement } from './stanza-element-048ee85e.js';
+
+async function qux(stanza, params) {
+  const dataUrl = params["data-url"];
+  const receivedData = await fetch(dataUrl).then((res) => res.json());
+  stanza.render({
+    template: "stanza.html.hbs",
+    parameters: {
+      dataUrl,
+      receivedData: JSON.stringify(receivedData, null, "  "),
+    },
+  });
+}
+
+var stanzaModule = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  'default': qux
+});
+
+var metadata = {
+	"@context": {
+	stanza: "http://togostanza.org/resource/stanza#"
+},
+	"@id": "qux",
+	"stanza:label": "Qux",
+	"stanza:definition": "",
+	"stanza:type": "Stanza",
+	"stanza:display": "Text",
+	"stanza:provider": "",
+	"stanza:license": "MIT",
+	"stanza:author": "Yoji Shidara",
+	"stanza:address": "dara@shidara.net",
+	"stanza:contributor": [
+],
+	"stanza:created": "2021-03-08",
+	"stanza:updated": "2021-03-08",
+	"stanza:parameter": [
+	{
+		"stanza:key": "data-url",
+		"stanza:type": "string",
+		"stanza:example": "https://quotes.rest/qod?language=en",
+		"stanza:description": "who to say hello to",
+		"stanza:required": false
+	}
+],
+	"stanza:about-link-placement": "bottom-right",
+	"stanza:style": [
+	{
+		"stanza:key": "--greeting-color",
+		"stanza:type": "color",
+		"stanza:default": "#eb7900",
+		"stanza:description": "text color of greeting"
+	},
+	{
+		"stanza:key": "--greeting-align",
+		"stanza:type": "single-choice",
+		"stanza:choice": [
+			"left",
+			"center",
+			"right"
+		],
+		"stanza:default": "center",
+		"stanza:description": "text align of greeting"
+	}
+],
+	"stanza:incomingEvent": [
+],
+	"stanza:outgoingEvent": [
+]
+};
+
+var templates = [
+  ["stanza.html.hbs", {"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<p class=\"greeting\">Loading from "
+    + alias4(((helper = (helper = lookupProperty(helpers,"dataUrl") || (depth0 != null ? lookupProperty(depth0,"dataUrl") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dataUrl","hash":{},"data":data,"loc":{"start":{"line":1,"column":33},"end":{"line":1,"column":44}}}) : helper)))
+    + "</p>\n<pre><code>"
+    + alias4(((helper = (helper = lookupProperty(helpers,"receivedData") || (depth0 != null ? lookupProperty(depth0,"receivedData") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"receivedData","hash":{},"data":data,"loc":{"start":{"line":2,"column":11},"end":{"line":2,"column":27}}}) : helper)))
+    + "</code></pre>";
+},"useData":true}]
+];
+
+var css = "/*\n\nYou can set up a global style here that is commonly used in each stanza.\n\nExample:\n\nh1 {\n  font-size: 24px;\n}\n\n*/\nmain {\n  padding: 1rem 2rem;\n}\n\np.greeting {\n  margin: 0;\n  font-size: 24px;\n  color: var(--greeting-color);\n  text-align: var(--greeting-align);\n}";
+
+defineStanzaElement({stanzaModule, metadata, templates, css, url: import.meta.url});
+//# sourceMappingURL=qux.js.map
