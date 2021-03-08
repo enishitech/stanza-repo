2ab193e91872daaff8a9c5479a70e7d3a3d1eda8
Yoji Shidara <dara@shidara.net>
2021-03-08 16:39:11 +0900

    Do not fetch if dataUrl is not given

--- stanzas/qux/index.js
+++ stanzas/qux/index.js
@@ -1,5 +1,8 @@
 export default async function qux(stanza, params) {
   const dataUrl = params["data-url"];
+  if (!dataUrl) {
+    return;
+  }
   const receivedData = await fetch(dataUrl).then((res) => res.json());
   stanza.render({
     template: "stanza.html.hbs",
