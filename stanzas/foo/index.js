6298c9c3088d3d765a5e005983e8c8d012ea0b2e
Yoji Shidara <dara@shidara.net>
2021-03-08 14:41:34 +0900

    Implement foo stanza to send events

--- stanzas/foo/index.js
+++ stanzas/foo/index.js
@@ -1,8 +1,13 @@
 export default async function foo(stanza, params) {
   stanza.render({
-    template: 'stanza.html.hbs',
-    parameters: {
-      greeting: `Hello, ${params['say-to']}!`
-    }
+    template: "stanza.html.hbs",
+    parameters: {},
+  });
+  const input = stanza.root.querySelector("input");
+  input.addEventListener("input", (ev) => {
+    const value = ev.target.value;
+    stanza.host.dispatchEvent(
+      new CustomEvent("valueChanged", { detail: { value } })
+    );
   });
 }
