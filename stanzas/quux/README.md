2a35dc94d09f1193b93338556f1fb2a858ef1269
Keita Urashima <ursm@ursm.jp>
2021-03-08 23:18:45 +0900

    Add quux stanza

---
+++ stanzas/quux/README.md
@@ -0,0 +1,3 @@
+# Quux
+
+Stanza description goes here. Edit `stanzas/quux/README.md` to update.
--- stanzas/quux/index.js
+++ stanzas/quux/index.js
@@ -1,14 +1,14 @@
-export default async function qux(stanza, params) {
+export default async function quux(stanza, params) {
   const dataUrl = params["data-url"];
   if (!dataUrl) {
     return;
   }
   const receivedData = await fetch(dataUrl).then((res) => res.json());
   stanza.render({
-    template: "stanza.html.hbs",
+    template: 'stanza.html.hbs',
     parameters: {
       dataUrl,
-      receivedData: JSON.stringify(receivedData.slice(0, 3), null, "  "),
-    },
+      receivedData
+    }
   });
 }
