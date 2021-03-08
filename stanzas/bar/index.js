47d8bf582cb570800097f086cb11cfcb0e343bb4
Yoji Shidara <dara@shidara.net>
2021-03-08 14:58:05 +0900

    Receive value changed event

--- stanzas/bar/index.js
+++ stanzas/bar/index.js
@@ -1,8 +1,12 @@
 export default async function bar(stanza, params) {
   stanza.render({
-    template: 'stanza.html.hbs',
+    template: "stanza.html.hbs",
     parameters: {
-      greeting: `Hello, ${params['say-to']}!`
-    }
+      greeting: `Hello, ${params["say-to"]}!`,
+    },
   });
+}
+
+export function handleEvent(stanza, params, event) {
+  console.log("bar: received", event);
 }
