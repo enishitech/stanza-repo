2713656da44865a78b61d2f4e1b6f333976eb431
darashi <darashi@users.noreply.github.com>
2021-03-08 07:40:14 +0000

    deploy: 2ab193e91872daaff8a9c5479a70e7d3a3d1eda8

--- qux.js
+++ qux.js
@@ -2,6 +2,9 @@ import { d as defineStanzaElement } from './stanza-element-048ee85e.js';
 
 async function qux(stanza, params) {
   const dataUrl = params["data-url"];
+  if (!dataUrl) {
+    return;
+  }
   const receivedData = await fetch(dataUrl).then((res) => res.json());
   stanza.render({
     template: "stanza.html.hbs",
